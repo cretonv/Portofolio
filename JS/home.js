2e12f14ce6c9a59765e58284bd0e99fc604b8938
Vincent <vincecreton@hotmail.fr>
2019-10-16 23:56:31 +0200

    Création et animation du bouton "burger"

---
+++ JS/home.js
@@ -0,0 +1,49 @@
+var lines = document.querySelectorAll(".burger-line-dark");
+var line1 = lines[0];
+var line2 = lines[1];
+var line3 = lines[2];
+
+var closes = document.querySelectorAll(".burger-close-wrap");
+var close1 = closes[0];
+var close2 = closes[1];
+
+function burger_action(){
+
+    if (line1.style.width == "0vh") {
+        line1.style.width = "5vh";
+        line1.style.transition = "width 0.9s 1s";
+    } else  {
+        line1.style.width = "0vh";
+        line1.style.transition = "width 0.9s";
+    }
+    if (line2.style.width == "0vh") {
+        line2.style.width = "5vh";
+        line2.style.transition = "width 0.7s 1s";
+    } else  {
+        line2.style.width = "0vh";
+        line2.style.transition = "width 0.7s";
+    }
+    if (line3.style.width == "0vh") {
+        line3.style.width = "5vh";
+        line3.style.transition = "width 0.5s 1s";
+    } else  {
+        line3.style.width = "0vh";
+        line3.style.transition = "width 0.5s";
+    }
+
+    if (close1.style.width == "0vh") {
+        close1.style.width = "3vh";
+        close1.style.transition = "width 0.7s 1s";
+    } else {
+        close1.style.width = "0vh";
+        close1.style.transition = "width 0.7s";
+    }
+    if (close2.style.width == "0vh") {
+        close2.style.width = "3vh";
+        close2.style.transition = "width 0.7s 1s";
+    } else {
+        close2.style.width = "0vh";
+        close2.style.transition = "width 0.7s";
+    }
+
+}
