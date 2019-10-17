15e60ebe58491fd07de6d3edae1c41f7c99e299a
Vincent <vincecreton@hotmail.fr>
2019-10-17 22:56:07 +0200

    Ajout del'apparition du menu

--- JS/home.js
+++ JS/home.js
@@ -7,9 +7,12 @@ var closes = document.querySelectorAll(".burger-close-wrap");
 var close1 = closes[0];
 var close2 = closes[1];
 
+var menu = document.querySelector("#menu");
+menu.open = false; // garde en mémoire l'état du menu
+
 function burger_action(){
 
-    if (line1.style.width == "0vh") {
+    if (line1.style.width == "0vh") {                       // animation des lignes du bouton
         line1.style.width = "5vh";
         line1.style.transition = "width 0.9s 1s";
     } else  {
@@ -31,7 +34,7 @@ function burger_action(){
         line3.style.transition = "width 0.5s";
     }
 
-    if (close1.style.width == "0vh") {
+    if (close1.style.width == "0vh") {              // animation de la croix du bouton
         close1.style.width = "3vh";
         close1.style.transition = "width 0.7s 1s";
     } else {
@@ -46,4 +49,12 @@ function burger_action(){
         close2.style.transition = "width 0.7s";
     }
 
+
+    if (!menu.open) {                           // si le menu est fermé
+        menu.classList.add('open')
+        menu.open = true
+    } else {                                    // Si le menu est ouvert
+        menu.classList.remove('open')        // On enlève la classe CSS
+        menu.open = false                           // On met à jour l'indicateur d'état
+    }
 }
