7f5f0e18ef66eff0f1f4fec049a5195e578e7c42
Vincent <vincecreton@hotmail.fr>
2019-10-21 21:25:02 +0200

    Animation au hover des éléments du menu

--- JS/home.js
+++ JS/home.js
@@ -1,13 +1,13 @@
-var lines = document.querySelectorAll(".burger-line-dark");
-var line1 = lines[0];
+var lines = document.querySelectorAll(".burger-line-dark"); // Sélectionne les lignes du boutons burger dans un taleau
+var line1 = lines[0];                                                // Associe chaque lignes à une variable
 var line2 = lines[1];
 var line3 = lines[2];
 
-var closes = document.querySelectorAll(".burger-close-wrap");
-var close1 = closes[0];
+var closes = document.querySelectorAll(".burger-close-wrap"); // Sélectionne la croix du boutons burger dans un taleau
+var close1 = closes[0];                                                // Associe chaque lignes de la croix à une variable
 var close2 = closes[1];
 
-var menu = document.querySelector("#menu");
+var menu = document.querySelector("#menu");           //Stock dans une variable la div contenant tout le menu
 menu.open = false; // garde en mémoire l'état du menu
 
 var top_bar = document.querySelector(".verti-top");
@@ -21,6 +21,16 @@ var middle_right_bar = document.querySelector(".menu-border-right");
 
 var bottom_left_bar = document.querySelector(".border-bottom-left");
 var bottom_right_bar = document.querySelector(".border-bottom-right");
+
+var menu_items = document.querySelectorAll('.menu-item');
+var menu1= menu_items[0];
+var menu2= menu_items[1];
+var menu3= menu_items[2];
+var menu4= menu_items[3];
+
+var reseau_item = document.querySelector(".reseau-element");
+
+
 
 function burger_action(){
 
@@ -63,7 +73,9 @@ function burger_action(){
 
 
     if (!menu.open) {                           // si le menu est fermé
-        menu.classList.add('open')
+        menu.classList.add('open')              // On fais apparaitre le menu
+
+        // Permet de créer l'animation du cadre qui se dessine
         top_bar.classList.add("top-apparition")
         bottom_bar.classList.add("top-apparition")
 
@@ -76,11 +88,19 @@ function burger_action(){
         bottom_right_bar.classList.add("middle-top-apparition")
         bottom_left_bar.classList.add("middle-top-apparition")
 
+        menu1.classList.add("menu-item-visible");       // Fais apparaitre les items du menu
+        menu2.classList.add("menu-item-visible");
+        menu3.classList.add("menu-item-visible");
+        menu4.classList.add("menu-item-visible");
+
+
         menu.open = true                        // On met à jour l'indicateur d'état
 
     } else {                                    // Si le menu est ouvert
 
-        menu.classList.remove('open')                    // On enlève la classe CSS
+        menu.classList.remove('open')                    // On enlève la classe CSS et fais disparaitre le menu
+
+
         top_bar.classList.remove("top-apparition")
         bottom_bar.classList.remove("top-apparition")
 
@@ -92,6 +112,7 @@ function burger_action(){
 
         bottom_right_bar.classList.remove("middle-top-apparition")
         bottom_left_bar.classList.remove("middle-top-apparition")
+
 
         menu.open = false                                       // On met à jour l'indicateur d'état
     }
