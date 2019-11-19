7335c38d2e8c70d69f8a3c98bb0d7171d83600a1
Vincent <vincecreton@hotmail.fr>
2019-11-19 08:52:41 +0100

    Animation du cadre pour les créa web

---
+++ JS/crea.js
@@ -0,0 +1,11 @@
+var border_right = document.querySelector('.bloc-border-right');
+var border_bottom = document.querySelector('.bloc-border-bottom');
+var border_left = document.querySelector('.bloc-border-left');
+var border_top = document.querySelector('.bloc-border-top');
+
+
+
+function anim_cadre(){
+    console.log('ça roule')
+    /*border_top.classList.add('bloc-border-top-anim');*/
+}
